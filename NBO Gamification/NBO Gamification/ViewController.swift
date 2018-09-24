53e474cb595dfdb9880b6071f885d412c4d832ae
facundop-eb <facundop@eventbrite.com>
2018-09-24 17:33:39 -0300

    upload base project

---
+++ NBO Gamification/NBO Gamification/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NBO Gamification
+//
+//  Created by facundop on 24/09/2018.
+//  Copyright © 2018 nbogamification. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
