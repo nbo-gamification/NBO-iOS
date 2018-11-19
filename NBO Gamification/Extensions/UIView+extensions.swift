f79b63f9c70392f062380feafb9bc5e2af7e1a62
eb-dante <dante@eventbrite.com>
2018-11-19 20:59:23 -0300

    adding login view

---
+++ NBO Gamification/Extensions/UIView+extensions.swift
@@ -0,0 +1,22 @@
+//
+//  UIView+extensions.swift
+//  NBO Gamification
+//
+//  Created by dante on 24/11/2018.
+//  Copyright © 2018 nbogamification. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+}
